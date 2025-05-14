f23558cd022578e50bc92bdce6075148a800a66c
osivaM <o.v.v.9322@gmail.com>
2025-05-14 19:35:38 +0500

    initial commit

---
+++ src/main/java/com/github/osipovvj/webrise_test_task/WebriseTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.github.osipovvj.webrise_test_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebriseTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebriseTestTaskApplication.class, args);
+    }
+
+}
